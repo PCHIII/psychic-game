41c7647fd75c1a28f75ee21db818aab1217a1b3c
PCHIII <treyhelmer@gmail.com>
2019-06-24 14:56:43 -0400

    game update, bg-image

---
+++ assets/javascript/main.js
@@ -0,0 +1,66 @@
+// letter guessing game, computer generates letter from array, user inputs a letter guess, user guesses <= 9 are displayed.
+// wins are added, losses are added, guess are deducted from 9.
+// alphabet array
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//scoring variables
+// user guess lists here a maximum of 9 times or until win.
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var guesses = [];
+
+//computer chooses a random letter
+var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+// reset game after win or loss
+
+function reset() {
+    reset == answer;
+
+    // console.log(reset);
+    guessesLeft = 9;
+    guesses = [];
+
+}
+
+// console.log(answer)
+
+//user chooses a letter via key event.
+document.onkeypress = function(event) {
+    // var userGuess = event.key;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    guesses.push(userGuess);
+
+    // if user guess matches computer guess, add to win.
+    //if win add plus 1 to win, reset.
+
+    if (userGuess == answer) {
+        wins++;
+        alert("Namaste, you are Enlightened") ;
+        reset();
+    }
+    //  if user guess is different then computer guess, deduct 1 unit from remaining guesses.
+    // if guesses left reaches zero then add loss.
+    //if loss, add plus 1 to loss, reset.
+    else {
+        guessesLeft--;
+
+}
+        if (guessesLeft == 0) {
+            losses++;
+            reset();
+
+        
+    }
+
+    //  add elements into html
+    document.getElementById('guesses').innerHTML = "Your Guesses so far: " + guesses.join("  ");
+    document.getElementById('wins').innerHTML = "Wins: " + wins;
+    document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
+}
+    
+    
+    
